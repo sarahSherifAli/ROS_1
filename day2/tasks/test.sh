6c5f1965d6d514204e0d15715b7fd8fc8aadc678
sarahSherifAli <sarahelngar@aucegypt.edu>
2020-08-13 00:53:59 +0200

    day2

---
+++ day2/tasks/test.sh
@@ -0,0 +1,13 @@
+echo "Hello! Your current directory is:"
+pwd
+echo "And the files in this direcotry are:"
+ls
+##sh files are unix (linux) shell executables files, they are the equivalent (but much more powerful) of bat files on windows.
+##So you need to run it from a linux console, just typing its name the same you do with bat files on windows
+
+#how to run
+##Open the terminal. Go to the directory where you want to create your script.
+##Create a file with . sh extension.
+##Write the script in the file using an editor.
+##Make the script executable with command chmod +x <fileName>.
+##Run the script using ./<fileName>.
